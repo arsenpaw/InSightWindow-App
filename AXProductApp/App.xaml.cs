e7475184d01df2a94222dc7fc812fbb502001b9e
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-03-04 16:05:47 +0200

    Add project files.

---
+++ AXProductApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AXProductApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ AXProductApp/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AXProductApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
