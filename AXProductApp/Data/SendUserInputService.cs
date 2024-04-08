b94708b941bad76e2f5e9ebfe570076e19ccc156
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-04-08 18:32:18 +0300

    rename service and register it

---
+++ AXProductApp/Data/SendUserInputService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXProductApp.Data
+{
+    public class SendUserInputService
+    {
+        UserInputStatus userInputStatus = new UserInputStatus(); 
+        public async Task SendOpenInfo(bool isOpened)
+        {
+            userInputStatus.IsOpen = isOpened;
+            Debug.WriteLine($"Is open: {userInputStatus.IsOpen}");
+        }
+
+        public async Task SendProtectedInfo(bool isProtected)
+        {
+            userInputStatus.isProtected = isProtected;
+            Debug.WriteLine($"Is protected: {userInputStatus.isProtected}");
+        }
+    }
+}
