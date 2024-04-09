6d07f111b4ba451feb72d4362776ab702a455e4a
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-04-09 18:03:29 +0300

    add send and connect function to hub

--- AXProductApp/Services/SendUserInputService.cs
+++ AXProductApp/Services/SendUserInputService.cs
@@ -1,25 +1,67 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AXProductApp.Data
 {
     public class SendUserInputService
     {
-        UserInputStatus userInputStatus = new UserInputStatus(); 
+        UserInputStatus userInputStatus = new UserInputStatus();
+        private HubConnection _hubConnection;
+        public  SendUserInputService()
+        {
+           InitiaizeConnection();
+        }
+        private async Task InitiaizeConnection()
+        {
+            _hubConnection = new HubConnectionBuilder()
+           .WithUrl(LinkToHub.ArsenTest)
+           .WithAutomaticReconnect()
+           .Build();
+            try
+            {
+                await _hubConnection.StartAsync();   
+
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine($"Error sending data tu hub {ex.Message}");
+            }
+        }
+
+       
         public async Task SendOpenInfo(bool isOpened)
         {
             userInputStatus.IsOpen = isOpened;
             Debug.WriteLine($"Is open: {userInputStatus.IsOpen}");
+            await sendDataToHub(userInputStatus);
         }
 
         public async Task SendProtectedInfo(bool isProtected)
         {
             userInputStatus.isProtected = isProtected;
             Debug.WriteLine($"Is protected: {userInputStatus.isProtected}");
+            await sendDataToHub(userInputStatus);
+        }
+        private async Task sendDataToHub(UserInputStatus userInputStatus)
+        {
+            try
+            {
+                if (_hubConnection.State == HubConnectionState.Connected)
+                    await _hubConnection.SendAsync("SaveUserInput", userInputStatus);
+                else
+                    await InitiaizeConnection();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error in sendind data to hub {ex.Message}");
+            }
         }
     }
 }
