83dade7d70533b8098b8dfc0afeba1b280692d71
Arsen Pavliuk <arsenpaw7@gmail.com>
2024-07-15 14:17:56 +0300

    add filtring by device type

---
+++ AXProductApp/Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations; 
+using System.Reflection.Metadata;
+using InSightWindowAPI.Models.Dto;
+
+namespace InSightWindowAPI.Models.DeviceModel
+{
+    public class Device
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get;  set; } = Guid.NewGuid();
+
+        public required string DeviceType { get; set; } 
+
+        public virtual Guid? UserId { get; set; }
+
+        public virtual User? User { get; set; }
+    }
+}
