ac12b5bdd9570183b9616d7310484bdcbd2535fe
Arsen Pavliuk <123877570+arsenpaw@users.noreply.github.com>
2024-12-08 12:47:04 +0200

    fix: send token only when login sucess

--- AXProductApp/Services/ManageFireBaseTokenService.cs
+++ AXProductApp/Services/ManageFireBaseTokenService.cs
@@ -6,42 +6,26 @@ namespace AXProductApp.Services
     public class ManageFireBaseTokenService : IManageFireBaseTokenService
     {
         private readonly AuthApiClient _authApiClient;
-
         private readonly ILocalStorageService _localStorageService;
+
         public ManageFireBaseTokenService(AuthApiClient authApiClient, ILocalStorageService localStorage)
         {
             _localStorageService = localStorage;
             _authApiClient = authApiClient;
         }
-        public async Task SendTokenToServer(string token)
+
+        private async Task SendTokenToServer(string token)
         {
             var userSecret = await _localStorageService.GetUserSecret();
             var resp = await _authApiClient.PostAsync<object>($"FireBaseTokens/{token}", null);
         }
 
-        public async Task CreateToken()
+        public async Task EnablePushNotificationForCurrentDevice()
         {
+            var firebaseToken = await CrossFirebaseCloudMessaging.Current.GetTokenAsync();
             await CrossFirebaseCloudMessaging.Current.CheckIfValidAsync();
-            try
-            {
-                var firebaseToken = await CrossFirebaseCloudMessaging.Current.GetTokenAsync();
-                if (!string.IsNullOrEmpty(firebaseToken))
-                {
-                    await SendTokenToServer(firebaseToken);
-                }
-                else
-                {
-                    throw new InvalidOperationException("Firebase token could not be retrieved");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
+            await SendTokenToServer(firebaseToken);
         }
     }
-
-
 }
 
