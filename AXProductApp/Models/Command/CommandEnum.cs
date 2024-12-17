fb4dd7430f9e8dee639e359e300cdfd309ef5072
YuraDnistriansky <yura.dniester@gmail.com>
2024-12-17 04:41:09 +0200

    Add Send commands to server

---
+++ AXProductApp/Models/Command/CommandEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace AXProductApp.Models.Command
+{
+    public enum CommandEnum
+    {
+        Open = 11,
+        Close = 10,
+        StopAlarm = 20,
+        StartAlarm = 21,
+        Stop = 30,
+
+    }
+}
--- AXProductApp/Services/WindowStatusService.cs
+++ AXProductApp/Services/WindowStatusService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using AXProductApp.Interfaces;
 using AXProductApp.Models;
+using AXProductApp.Models.Command;
 using AXProductApp.Services;
 using Microsoft.AspNetCore.SignalR.Client;
 using Newtonsoft.Json;
@@ -12,10 +13,11 @@ namespace AXProductApp.Data;
 public class ReceiveWindowStatusService : IReceiveWindowStatusService
 {
     public HubConnection _hubConnection;
+
     private bool prevAlarmTriggered;
 
     public event Action<AllWindowDataDto> DataReceived;
-
+    public event Action<UserInputStatus> UserReceived;
 
     public async Task<bool> InitializeConnectionAsync(Guid deviceId)
     {
@@ -33,8 +35,6 @@ public class ReceiveWindowStatusService : IReceiveWindowStatusService
 
     public async Task ReceiveSensorData(Guid deviceId)
     {
-        await App.Current.MainPage.DisplayAlert("Ok", "To do", "Method ok");
-
         _hubConnection.On<AllWindowDataDto>("ReceiveSensorData", async status =>
         {
 
@@ -56,23 +56,21 @@ public class ReceiveWindowStatusService : IReceiveWindowStatusService
         });
     }
 
-    public async Task ReceiveSensorDataTest(Guid deviceId)
+
+    public async Task SendCommand(Guid deviceId, CommandDto command)
     {
-        // Перенесені значить на сторінку
-        AllWindowDataDto statusTest = new AllWindowDataDto();
-        statusTest.Temperature = 10;
-        statusTest.Humidity = 10;
-        statusTest.IsRain = false;
-        statusTest.IsOpen = true;
-        statusTest.IsAlarm = true;
-        DataReceived?.Invoke(statusTest);
+        if (_hubConnection.State == HubConnectionState.Connected)
+        {
+            await App.Current.MainPage.DisplayAlert("Ok", "Ok", "Ok");
+            await _hubConnection.SendAsync("SendCommandToEsp32", deviceId, command);
+        }
+        else
+        {
+            Debug.WriteLine("Error: No cotect to hub");
+            await App.Current.MainPage.DisplayAlert("Oops", "An error occurred while sendig youre command ", "Ok");
+        }
     }
 
-
-    public async Task OpenCloseWindowCommand(Guid deviceId)
-    {
-
-    }
 
     public async Task StopConnection(Guid deviceId)
     {
@@ -87,18 +85,33 @@ public class ReceiveWindowStatusService : IReceiveWindowStatusService
 
 
 
-    public async Task TryShowDataFromCashe(string deviceId)
+
+
+    public async Task ReceiveSensorDataTest(Guid deviceId)
     {
-        var strData = await SecureStorage.GetAsync(deviceId);
-        if (strData != null)
-        {
-            var objDataFromCache = JsonConvert.DeserializeObject<AllWindowDataDto>(strData);
-            DataReceived?.Invoke(objDataFromCache);
-        }
+        // Перенесені значить на сторінку
+        AllWindowDataDto statusTest = new AllWindowDataDto();
+        statusTest.Temperature = 10;
+        statusTest.Humidity = 10;
+        statusTest.IsRain = false;
+        statusTest.IsOpen = true;
+        statusTest.IsAlarm = true;
+        DataReceived?.Invoke(statusTest);
     }
 
 
 
+
+
+    //public async Task TryShowDataFromCashe(string deviceId)
+    //{
+    //    var strData = await SecureStorage.GetAsync(deviceId);
+    //    if (strData != null)
+    //    {
+    //        var objDataFromCache = JsonConvert.DeserializeObject<AllWindowDataDto>(strData);
+    //        DataReceived?.Invoke(objDataFromCache);
+    //    }
+    //}
 
 
     //public async Task OnAppUpdate()
